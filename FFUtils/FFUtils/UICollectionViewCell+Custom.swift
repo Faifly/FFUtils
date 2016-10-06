1cd454542e98c32f79b86e3d1c5b39669a917c40
Artem Kalmykov <ar.kalmykov@gmail.com>
2016-10-06 14:32:30 +0300

    Implemented UICollectionViewCell + reuseID extension.

---
+++ FFUtils/FFUtils/UICollectionViewCell+Custom.swift
@@ -0,0 +1,17 @@
+//
+//  UICollectionViewCell+Custom.swift
+//  FFUtils
+//
+//  Created by Artem Kalmykov on 10/6/16.
+//  Copyright © 2016 Faifly. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionViewCell
+{
+    public static var reuseID: String
+    {
+        return String(describing: self)
+    }
+}
