b20f5dee82c2b4219c5d0569b920ebc617c306ae
Artem Kalmykov <ar.kalmykov@yahoo.com>
2017-10-18 13:45:04 +0300

    Add optional comparison operator overloads.

--- FFUtils/FFUtils/Utils.swift
+++ FFUtils/FFUtils/Utils.swift
@@ -9,3 +9,63 @@
 import Foundation
 
 public typealias EmptyClosure = (() -> Void)
+
+func ><T: Equatable>(lhs: T?, rhs: T?) -> Bool
+{
+    switch (lhs, rhs)
+    {
+    case let (l?, r?):
+        return l > r
+    case (.none, .none):
+        return false
+    case (.none, _?):
+        return false
+    case (_?, .none):
+        return true
+    }
+}
+
+func >=<T: Equatable>(lhs: T?, rhs: T?) -> Bool
+{
+    switch (lhs, rhs)
+    {
+    case let (l?, r?):
+        return l >= r
+    case (.none, .none):
+        return false
+    case (.none, _?):
+        return false
+    case (_?, .none):
+        return true
+    }
+}
+
+func <<T: Equatable>(lhs: T?, rhs: T?) -> Bool
+{
+    switch (lhs, rhs)
+    {
+    case let (l?, r?):
+        return l < r
+    case (.none, .none):
+        return false
+    case (.none, _?):
+        return true
+    case (_?, .none):
+        return false
+    }
+}
+
+func <=<T: Equatable>(lhs: T?, rhs: T?) -> Bool
+{
+    switch (lhs, rhs)
+    {
+    case let (l?, r?):
+        return l <= r
+    case (.none, .none):
+        return false
+    case (.none, _?):
+        return true
+    case (_?, .none):
+        return false
+    }
+}
